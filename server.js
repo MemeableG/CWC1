d536af94e6325590ccaf5d6a510048f486e2b3e8
MemeableG <157776371+MemeableG@users.noreply.github.com>
2024-03-28 23:34:19 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,1 @@
-const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 3000 });
 
-wss.on('connection', function connection(ws) {
-    ws.on('message', function incoming(message) {
-        wss.clients.forEach(function each(client) {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(message);
-            }
-        });
-    });
-});
